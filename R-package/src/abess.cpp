04b9ad8801132d30fc9bde850555277c980b5a9f
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2021-03-07 01:52:01 +0800

    to pass website build

--- R-package/src/abess.cpp
+++ R-package/src/abess.cpp
@@ -27,6 +27,21 @@ using namespace Rcpp;
 #ifdef _OPENMP
 #include <omp.h>
 // [[Rcpp::plugins(openmp)]]
+#else
+
+#ifndef DISABLE_OPENMP
+// use pragma message instead of warning
+#pragma message("Warning: OpenMP is not available, "                    \
+"project will be compiled into single-thread code. "                    \
+"Use OpenMP-enabled compiler to get benefit of multi-threading.")
+#endif
+
+inline int omp_get_thread_num() { return 0; }
+inline int omp_get_num_threads() { return 1; }
+inline int omp_get_num_procs() { return 1; }
+inline void omp_set_num_threads(int nthread) {}
+inline void omp_set_dynamic(int flag) {}
+  
 #endif
 
 // #ifdef OTHER_ALGORITHM2
