9248764fcecf1c82302f86da1fa778322b259ba1
Jiang-Kangkang <983286638@qq.com>
2021-05-09 19:15:40 +0800

    update

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../abess'))
+# sys.path.insert(0, os.path.abspath('../../abess'))
 
 
 # -- Project information -----------------------------------------------------
