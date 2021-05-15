26c386f3059546c39e391d3acd30aca5b4a398a8
Jiang-Kangkang <983286638@qq.com>
2021-05-15 22:28:00 +0800

    update

--- python/pytest/test_all.py
+++ python/pytest/test_all.py
@@ -25,7 +25,7 @@ class TestClass:
         data = gen_data(n, p, family=family, k=k, rho=rho, sigma=sigma)
         sequence = range(0, int(n/np.log(np.log(n)) / np.log(p)))
 
-        model = abessLm(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=False, screening_size=30,
+        model = abessLm(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=True, screening_size=50,
                         K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
                         exchange_num=2, tau=0.1 * np.log(n*p) / n,
                         primary_model_fit_max_iter=10, primary_model_fit_epsilon=1e-6, early_stop=False, approximate_Newton=True, ic_coef=1., thread=5, covariance_update=False)
@@ -81,68 +81,68 @@ class TestClass:
         # assert model2.beta[nonzero_fit] == approx(
         #     reg.coef_[0], rel=1e-2, abs=1e-2)
 
-    # def test_cox(self):
-    #     n = 1000
-    #     p = 200
-    #     k = 20
-    #     family = "cox"
-    #     rho = 0.5
-    #     sigma = 1
+    def test_cox(self):
+        n = 1000
+        p = 200
+        k = 20
+        family = "cox"
+        rho = 0.5
+        sigma = 1
 
-    #     np.random.seed(1)
-    #     data = gen_data(n, p, family=family, k=k, rho=rho, sigma=sigma)
-    #     sequence = range(1, int(n/np.log(np.log(n)) / np.log(p)))
+        np.random.seed(1)
+        data = gen_data(n, p, family=family, k=k, rho=rho, sigma=sigma)
+        sequence = range(1, 21)
 
-    #     # model = abessCox(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=False, screening_size=30,
-    #     #                  K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
-    #     #                  exchange_num=2, tau=0.1 * np.log(n*p) / n,
-    #     #                  primary_model_fit_max_iter=30, primary_model_fit_epsilon=1e-6, early_stop=False, approximate_Newton=True, ic_coef=1., thread=5)
-    #     # start = time()
-    #     # group = np.linspace(1, p, p)
-    #     # model.fit(data.x, data.y, group=group)
-    #     # stop = time()
+        # model = abessCox(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=False, screening_size=30,
+        #                  K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
+        #                  exchange_num=2, tau=0.1 * np.log(n*p) / n,
+        #                  primary_model_fit_max_iter=30, primary_model_fit_epsilon=1e-6, early_stop=False, approximate_Newton=True, ic_coef=1., thread=5)
+        # start = time()
+        # group = np.linspace(1, p, p)
+        # model.fit(data.x, data.y, group=group)
+        # stop = time()
 
-    #     model2 = abessCox(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=False, screening_size=30,
-    #                       K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
-    #                       exchange_num=2, tau=0.1 * np.log(n*p) / n,
-    #                       primary_model_fit_max_iter=60, primary_model_fit_epsilon=1e-6, early_stop=False, approximate_Newton=False, ic_coef=1., thread=5)
-    #     group = np.linspace(1, p, p)
-    #     model2.fit(data.x, data.y, group=group)
+        model2 = abessCox(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=False, screening_size=30,
+                          K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
+                          exchange_num=2, tau=0.1 * np.log(n*p) / n,
+                          primary_model_fit_max_iter=60, primary_model_fit_epsilon=1e-6, early_stop=False, approximate_Newton=False, ic_coef=1., thread=5)
+        group = np.linspace(1, p, p)
+        model2.fit(data.x, data.y, group=group)
 
-    #     nonzero_true = np.nonzero(data.beta)[0]
-    #     nonzero_fit = np.nonzero(model2.beta)[0]
-    #     print(nonzero_true)
-    #     print(nonzero_fit)
+        nonzero_true = np.nonzero(data.beta)[0]
+        nonzero_fit = np.nonzero(model2.beta)[0]
+        print(nonzero_true)
+        print(nonzero_fit)
 
-    #     new_x = data.x[:, nonzero_fit]
+        # new_x = data.x[:, nonzero_fit]
 
-    #     survival = pd.DataFrame()
-    #     for i in range(new_x.shape[1]):
-    #         survival["Var" + str(i)] = new_x[:, i]
+        # survival = pd.DataFrame()
+        # for i in range(new_x.shape[1]):
+        #     survival["Var" + str(i)] = new_x[:, i]
 
-    #     survival["T"] = data.y[:, 0]
-    #     survival["E"] = data.y[:, 1]
+        # survival["T"] = data.y[:, 0]
+        # survival["E"] = data.y[:, 1]
 
-    #     cph = CoxPHFitter(penalizer=0, l1_ratio=0)
+        # cph = CoxPHFitter(penalizer=0, l1_ratio=0)
 
-    #     cph.fit(survival, 'T', event_col='E')
-    #     print(model2.beta[nonzero_fit])
-    #     print(cph.params_.values)
+        # cph.fit(survival, 'T', event_col='E')
+        # print(model2.beta[nonzero_fit])
+        # print(cph.params_.values)
 
-    #     assert model2.beta[nonzero_fit] == approx(
-    #         cph.params_.values, rel=2e-1, abs=2e-1)
+        # assert model2.beta[nonzero_fit] == approx(
+        #     cph.params_.values, rel=2e-1, abs=2e-1)
 
     def test_poisson(self):
         # to do
         n = 1000
-        p = 400
-        k = 20
-        family = "binomial"
+        p = 100
+        k = 10
+        family = "poisson"
         rho = 0.5
         sigma = 1
         np.random.seed(3)
         data = gen_data(n, p, family=family, k=k, rho=rho, sigma=sigma)
-        sequence = range(0, int(n/np.log(np.log(n)) / np.log(p)))
+        sequence = range(0, 11)
 
         # model = abessLogistic(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=False, screening_size=30,
         #                       K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
@@ -171,3 +171,54 @@ class TestClass:
         assert (nonzero_true == nonzero_fit).all()
         # assert model2.beta[nonzero_fit] == approx(
         #     reg.coef_[0], rel=1e-2, abs=1e-2)
+
+    # def test_mulgaussian(self):
+    #     n = 1000
+    #     p = 200
+    #     k = 20
+    #     family = "gaussian"
+    #     rho = 0.5
+    #     sigma = 1
+    #     np.random.seed(1)
+    #     data = gen_data(n, p, family=family, k=k, rho=rho, sigma=sigma)
+    #     sequence = range(0, int(n/np.log(np.log(n)) / np.log(p)))
+
+    #     model = abessLm(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=True, screening_size=50,
+    #                     K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
+    #                     exchange_num=2, tau=0.1 * np.log(n*p) / n,
+    #                     primary_model_fit_max_iter=10, primary_model_fit_epsilon=1e-6, early_stop=False, approximate_Newton=True, ic_coef=1., thread=5, covariance_update=False)
+    #     group = np.linspace(1, p, p)
+    #     model.fit(data.x, data.y, group=group)
+
+    #     nonzero_true = np.nonzero(data.beta)[0]
+    #     nonzero_fit = np.nonzero(model.beta)[0]
+    #     print(nonzero_true)
+    #     print(nonzero_fit)
+    #     # new_x = data.x[:, nonzero_fit]
+    #     # reg = linear_model.LinearRegression()
+    #     # reg.fit(new_x, data.y)
+    #     # assert model.beta[nonzero_fit] == approx(reg.coef_, rel=1e-5, abs=1e-5)
+    #     # assert (nonzero_true == nonzero_fit).all()
+
+    # def test_mulnomial(self):
+    #     n = 1000
+    #     p = 200
+    #     k = 20
+    #     family = "gaussian"
+    #     rho = 0.5
+    #     sigma = 1
+    #     np.random.seed(1)
+    #     data = gen_data(n, p, family=family, k=k, rho=rho, sigma=sigma)
+    #     sequence = range(0, int(n/np.log(np.log(n)) / np.log(p)))
+
+    #     model = abessLm(path_type="seq", sequence=sequence, ic_type='ebic', is_screening=True, screening_size=50,
+    #                     K_max=10, epsilon=10, powell_path=2, s_min=1, s_max=p, lambda_min=0.01, lambda_max=100, is_cv=True, K=5,
+    #                     exchange_num=2, tau=0.1 * np.log(n*p) / n,
+    #                     primary_model_fit_max_iter=10, primary_model_fit_epsilon=1e-6, early_stop=False, approximate_Newton=True, ic_coef=1., thread=5, covariance_update=False)
+    #     group = np.linspace(1, p, p)
+    #     model.fit(data.x, data.y, group=group)
+
+    #     nonzero_true = np.nonzero(data.beta)[0]
+    #     nonzero_fit = np.nonzero(model.beta)[0]
+    #     print(nonzero_true)
+    #     print(nonzero_fit)
