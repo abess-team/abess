ba6f086387cb1adacb9a4ed877055cf033b6a530
Jiang-Kangkang <983286638@qq.com>
2021-05-16 21:58:03 +0800

    add python file to coverage

--- python/abess/__init__.py
+++ python/abess/__init__.py
@@ -3,7 +3,7 @@
 # @Time    :
 # @Author  :
 # @Site    :
-# @File    : __init__.py.py
+# @File    : __init__.py
 
 # from abess.linear import PdasLm, PdasLogistic, PdasPoisson, PdasCox, L0L2Lm, L0L2Logistic, L0L2Poisson, L0L2Cox, GroupPdasLm, GroupPdasLogistic, GroupPdasPoisson, GroupPdasCox, abessLogistic
 from abess.linear import abessLogistic, abessLm, abessCox, abessPoisson, abessMLm, abessMultinomial
