16c5fc25e17024c49eb84f4b919a726969a7ed55
oooo26 <1160141009@qq.com>
2021-06-12 10:13:49 +0800

    update test

--- python/pytest/test_all.py
+++ python/pytest/test_all.py
@@ -281,3 +281,39 @@ class TestClass:
         # reg.fit(new_x, data.y)
         # assert model.coef_[nonzero_fit] == approx(reg.coef_, rel=1e-5, abs=1e-5)
         assert (nonzero_true == nonzero_fit).all()
+
+    def test_PCA(self):
+        n = 1000
+        p = 20
+        group_size = 5
+        group_num = 4
+
+        np.random.seed(2)
+        x1 = np.random.randn(n, 1)
+        x1 /= np.linalg.norm(x1)
+        X = x1.dot(np.random.randn(1, p)) + 0.01 * np.random.randn(n, p)
+        X = X - X.mean(axis = 0)
+        g_index = np.arange(group_num)
+        g_index = g_index.repeat(group_size)
+
+        # give X
+        model = abessPCA(support_size = range(0, p + 1))
+        model.fit(X)
+        coef1 = np.nonzero(model.coef_)[0]
+
+        # give Sigma
+        model.fit(Sigma = X.T.dot(X))
+        coef2 = np.nonzero(model.coef_)[0]
+
+        # check1:
+        check1 = (coef1 == coef2).all()
+
+        # group 
+        model = abessPCA(support_size = range(0, 4))
+        model.fit(X, group = g_index, is_normal=False)
+
+        # check2:
+        coef3 = np.unique(g_index[np.nonzero(model.coef_)])
+        check2 = (coef3.size <= 3)
+
+        assert (check1 and check2)
