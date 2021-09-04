8d22ee867a75cb28c0fe21d545cbe03b08d68490
Jiang-Kangkang <42855591+Jiang-Kangkang@users.noreply.github.com>
2021-09-04 10:42:23 +0800

    update package version

--- python/setup.py
+++ python/setup.py
@@ -62,7 +62,7 @@ with open(path.join(CURRENT_DIR, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='abess',
-      version='0.0.3',
+      version='0.0.4',
       author="Jin Zhu, Kangkang Jiang, Junhao Huang, Yanhang Zhang, Junxian Zhu, Xueqin Wang",
       author_email="zhuj37@mail2.sysu.edu.cn",
       maintainer="Kangkang Jiang",
