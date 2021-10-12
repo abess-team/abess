5ec1ba092c6cab1ab06ad6f3f01da1287d4a18bc
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2021-10-12 22:52:21 +0800

    Update test-abess.R

--- R-package/tests/testthat/test-abess.R
+++ R-package/tests/testthat/test-abess.R
@@ -484,41 +484,43 @@ test_that("abess (gamma) works", {
   p <- 100
   support.size <- 3
   dataset <- generate.data(n, p, support.size,
-                           family = "gamma", seed = 3)
+                           family = "gamma", seed = 2)
+  
+  # abess_fit <- abess(
+  #   dataset[["x"]],
+  #   dataset[["y"]],
+  #   family = "gamma",
+  #   tune.type = "cv",
+  #   #newton = "exact",
+  #   newton.thresh = 1e-8,
+  #   support.size = support.size,
+  #   always.include = which(dataset[["beta"]] != 0), 
+  #   important.search = p
+  # )
+  # 
+  # ## support size
+  # fit_s_size <- abess_fit[["best.size"]]
+  # coef_value <- coef(abess_fit, support.size = fit_s_size)
+  # est_index <- coef_value@i[-1]
+  # true_index <- which(dataset[["beta"]]!= 0)
+  # 
+  # #cat("ture:",true_index)
+  # #cat(";value:",dataset[["beta"]][true_index])
+  # #cat(";est:",est_index)
+  # 
+  # test_batch(abess_fit, dataset, Gamma)
   
   abess_fit <- abess(
     dataset[["x"]],
     dataset[["y"]],
     family = "gamma",
-    tune.type = "cv",
-    #newton = "exact",
-    newton.thresh = 1e-8,
-    support.size = support.size,
-    always.include = which(dataset[["beta"]] != 0)
-  )
-  
-  ## support size
-  fit_s_size <- abess_fit[["best.size"]]
-  coef_value <- coef(abess_fit, support.size = fit_s_size)
-  est_index <- coef_value@i[-1]
-  true_index <- which(dataset[["beta"]]!= 0)
-  
-  #cat("ture:",true_index)
-  #cat(";value:",dataset[["beta"]][true_index])
-  #cat(";est:",est_index)
-  
-  test_batch(abess_fit, dataset, Gamma)
-  
-  
-  abess_fit <- abess(
-    dataset[["x"]],
-    dataset[["y"]],
-    family = "gamma",
-    tune.type = "cv",
     #newton = "exact",
     newton.thresh = 1e-8,
     support.size = 0:support.size
   )
   plot(abess_fit, type = "tune")
   
+  test_batch(abess_fit, dataset, Gamma)
+  
+  which(as.vector(coef(abess_fit, 3))[-1] != 0) == which(dataset[["beta"]] != 0)
 })
