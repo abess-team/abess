e3df5125f16b0ae9128dd041e341c916993872d5
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2021-12-08 20:50:00 +0800

    update its document

--- R-package/R/generate.spc.matrix.R
+++ R-package/R/generate.spc.matrix.R
@@ -5,15 +5,18 @@
 #'
 #' @inheritParams generate.matrix
 #' @param support.size A integer specify the number of non-zero entries in the first column of loading matrix.
-#' @param sigma A numerical vector specify the standard deviation of each columns.
-#' Default \code{sigma = NULL} implies it is determined by \code{snr}.
+#' @param sparse.loading A \code{p}-by-\code{p} sparse orthogonal matrix. 
+#' If it is supplied, \code{support.size} would be omit. 
+#' @param sigma A numerical vector with length \code{p} specify the standard deviation of each columns.
+#' Default \code{sigma = NULL} implies it is determined by \code{snr}. 
+#' If it is supplied, \code{support.size} would be omit. 
 #'
 #' @return A \code{list} object comprising:
 #' \item{x}{An \eqn{n}-by-\eqn{p} matrix.}
-#' \item{coef}{The sparse loading matrix.}
-#' \item{coef}{A vector recording the number of non-zero entries.}
+#' \item{coef}{The sparse loading matrix used to generate x.}
+#' \item{support.size}{A vector recording the number of non-zero entries in each .}
 #'
-#' @details The methods for generating the matrix is detailedly described in the APPENDIX A. Data generation section in Schipper et al (2021).
+#' @details The methods for generating the matrix is detailedly described in the APPENDIX A: Data generation Section in Schipper et al (2021).
 #'
 #' @references Model selection techniques for sparse weight-based principal component analysis. de Schipper, Niek C and Van Deun, Katrijn. Journal of Chemometrics. 2021. \doi{10.1002/cem.3289}.
 #'
