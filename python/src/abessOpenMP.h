f4c9b1cd08b1feb46afd85cde0108a2f781ba6fd
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2021-12-09 10:50:52 +0800

    Update abessOpenMP.h
    sync from R-package

--- python/src/abessOpenMP.h
+++ python/src/abessOpenMP.h
@@ -6,15 +6,14 @@
 // [[Rcpp::plugins(openmp)]]
 #else
 #ifndef DISABLE_OPENMP
+#ifndef R_BUILD
 // use pragma message instead of warning
 #pragma message("Warning: OpenMP is not available, "                 \
                 "project will be compiled into single-thread code. " \
                 "Use OpenMP-enabled compiler to get benefit of multi-threading.")
 #endif
-inline int omp_get_thread_num()
-{
-    return 0;
-}
+#endif
+inline int omp_get_thread_num() { return 0; }
 inline int omp_get_num_threads() { return 1; }
 inline int omp_get_num_procs() { return 1; }
 inline void omp_set_num_threads(int nthread) {}
