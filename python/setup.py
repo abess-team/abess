8caf5c195659ea906f711486c867e694a1ee3a06
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2022-03-09 10:22:45 +0800

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -103,21 +103,21 @@ elif sys.platform.startswith('darwin'):
 else:
     eigen_path = CURRENT_DIR + "/include"
     extra_compile_args = [
-        "-DNDEBUG", "-O2",
-        "-Wall", "-std=c++11",
-        "-Wno-int-in-bool-context",
-        "-mavx", 
-        "-mfma", 
-        "-march=native"
-    ]
-    # x64_extra_compile_args=[
-    # ]
-    # if platform.processor() in ('x86_64'):
-    #     extra_compile_args.extend(x64_extra_compile_args)
-    #     pass
+        "-DNDEBUG", "-fopenmp",
+        "-O2", "-Wall",
+        "-std=c++11", "-mavx",
+        "-mfma", "-march=native",
+        # "-Wno-unused-variable",
+        # "-Wno-unused-but-set-variable",
+        "-Wno-int-in-bool-context"  # avoid warnings from Eigen
+    ],
     pybind_cabess_module = Pybind11Extension(
         name='pybind_cabess',
-        sources=[CURRENT_DIR + '/src/pywrap.cpp'],
+        sources=[CURRENT_DIR + '/src/api.cpp',
+                 CURRENT_DIR + '/src/List.cpp',
+                 CURRENT_DIR + '/src/utilities.cpp',
+                 CURRENT_DIR + '/src/normalize.cpp',
+                 CURRENT_DIR + '/src/pywrap.cpp'],
         extra_compile_args=extra_compile_args,
         extra_link_args=['-lgomp'],
         include_dirs=[
