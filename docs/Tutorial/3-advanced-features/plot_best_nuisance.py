dc25b5ed40cec03c81ab1d9cae87a2c52df57096
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2022-03-20 20:45:57 +0800

    update docs

--- docs/Tutorial/3-advanced-features/plot_best_nuisance.py
+++ docs/Tutorial/3-advanced-features/plot_best_nuisance.py
@@ -13,47 +13,54 @@ Nuisance Regression
 # ------------
 # Nuisance regression refers to best subset selection with some prior information that some variables are required to stay in the active set.
 # For example, if we are interested in a certain gene and want to find out what other genes are associated with the response when this particular gene shows effect.
+# The nuisance selection can be achieved by solving:
+#
+# .. math::
+#     \min_{(\beta, \gamma) \in \mathbb{R}^p} \frac{1}{2n} ||y-X (\beta^\top, \gamma^\top)^\top||_2^2,\; \textup{s.t.} \ ||\beta||_{0}\leq s .
+# 
+# Note that, the sparsity constraint restricts on :math:`\beta` and not on :math:`\gamma`. The effect of :math:`\gamma` corresponds to 
+# variables that stay in the active set.
 # 
 # Using: nuisance regression
 # --------------------------
 # In the ``LinearRegression()`` (or other methods), the argument ``always_select`` is designed to realize this goal.
-# User can pass a vector containing the indexes of the target variables to
-# ``always_select``. Here is an example.
-
+# Users can pass a list containing the indexes of the target variables to
+# ``always_select``. 
+# 
+# Here is an example demonstrating the advantage of nuisance selection.
+# We generate a high-dimensional dataset whose predictors are highly correlated (pairwise correlation: 0.6) 
+# and the effect of predictors on response is weaker than noise. 
+# 
 import numpy as np
 from abess.datasets import make_glm_data
-from abess.linear import LinearRegression
 
-np.random.seed(0)
-
-# gene data
-n = 100
-p = 20
-k = 5
-data = make_glm_data(n=n, p=p, k=k, family='gaussian')
-print('real coefficients:\n', data.coef_, '\n')
-print('real coefficients\' indexes:\n', np.nonzero(data.coef_)[0])
-
-model = LinearRegression(support_size=range(0, 6))
-model.fit(data.x, data.y)
-print('fitted coefficients:\n', model.coef_, '\n')
-print('fitted coefficients\' indexes:\n', np.nonzero(model.coef_)[0])
+# generate dataset
+np.random.seed(12345)
+data = make_glm_data(n=100, p=500, k=3, rho=0.6, family='gaussian', snr=0.5)
+print('True effective subset: ', np.nonzero(data.coef_))
 
 # %%
-# The coefficients are located in \[2, 5, 10, 11, 18\].
-# But if we suppose that the 7th and 8th variables are worthy to be
+# We use the standard ``abess`` to tackle this dataset:  
+#  
+from abess.linear import LinearRegression
+model = LinearRegression(support_size=range(10))
+model.fit(data.x, data.y)
+print('Estimated subset:', np.nonzero(model.coef_))
+
+# %%
+# The result from ``model`` omits the 449-th predictor belonging to the true effective set. 
+# But if we suppose that the 449-th predictor are worthy to be
 # included in the model, we can call:
 
-model = LinearRegression(support_size=range(0, 6), always_select=[7, 8])
+model = LinearRegression(support_size=range(10), always_select=[449])
 model.fit(data.x, data.y)
-print('fitted coefficients:\n', model.coef_, '\n')
-print('fitted coefficients\' indexes:\n', np.nonzero(model.coef_)[0])
+print('Estimated subset:', np.nonzero(model.coef_))
 
 # %%
-# Now the variables we chosen are always in the model.
+# Now the estimated subset is the same as the true effective set. 
+# The comparison between nuisance selection and standard ABESS suggests that 
+# reasonably leveraging prior information promotes the quality of subset selection.
 #
 # The ``abess`` R package also supports nuisance regression.
 # For R tutorial, please view
 # https://abess-team.github.io/abess/articles/v07-advancedFeatures.html.
-
-# sphinx_gallery_thumbnail_path = '_static/nuisance_cover.png'
--- docs/Tutorial/3-advanced-features/plot_best_regularized.py
+++ docs/Tutorial/3-advanced-features/plot_best_regularized.py
@@ -10,17 +10,17 @@ Regularized Best Subset Selection
 
 # %%
 # In some cases, especially under low signal-to-noise ratio (SNR) setting or predictors are highly correlated,
-# the vallina type of :math:`L_0` constrained model may not be satisfying and a more sophisticated trade-off between bias and variance is needed.
-# Under this concern, the ``abess`` package provides option of best subset selection with :math:`L_2` norm regularization called the regularized bess.
+# the vallina type of :math:`\ell_0` constrained model may not be satisfying and a more sophisticated trade-off between bias and variance is needed.
+# Under this concern, the ``abess`` package provides option of best subset selection with :math:`\ell_2` norm regularization called the regularized best-subset selection (RBESS).
 # The model has this following form:
 #
 # .. math::
-#     \arg\min_\beta L(\beta) + \alpha \|\beta\|_2^2.
-#
-# To implement the regularized bess, user need to specify a value to an additive argument ``alpha`` in the ``LinearRegression()`` function (or other methods).
+#     \arg\min_\beta L(\beta) + \alpha \|\beta\|_2^2,\; \textup{s.t.}\ ||\beta||_{0}\leq s.
+# 
+# To implement the RBESS, user need to specify a value to an additive argument ``alpha`` in the ``LinearRegression()`` function (or other methods).
 # This value corresponds to the penalization parameter in the model above.
 #
-# Let’s test the regularized best subset selection against the no-regularized one over 100 replicas in terms of prediction performance.
+# Let’s test the RBESS against the no-regularized one over 100 replicas in terms of prediction performance.
 # With argument ``snr`` in ``make_glm_data()``, we can add white noise
 # into generated data.
 import numpy as np
@@ -56,6 +56,4 @@ print("The average predition error under regularized best-subset selection:", np
 # The ``abess`` R package also supports regularized best-subset selection.
 # For R tutorial, please view
 # https://abess-team.github.io/abess/articles/v07-advancedFeatures.html.
-# 
-# sphinx_gallery_thumbnail_number = 1
 # 
