71dba4d2b4d382207c620826060fd03c8cc181be
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2022-03-22 23:16:29 +0800

    Update __init__.py

--- python/abess/__init__.py
+++ python/abess/__init__.py
@@ -5,7 +5,7 @@
 # @Site    :
 # @File    : __init__.py
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 __author__ = ("Jin Zhu, Kangkang Jiang, "
               "Junhao Huang, Yanhang Zhang, "
               "Yanhang Zhang, Shiyun Lin, "
