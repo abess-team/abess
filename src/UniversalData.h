26d157e0dfb8a4fa7aa6c06a3f50ef96fe585a41
bbayukari <17bbayukari@gmail.com>
2022-03-27 16:05:48 +0800

    update comments

--- src/UniversalData.h
+++ src/UniversalData.h
@@ -20,13 +20,13 @@ protected:
     Eigen::VectorXi effective_para_index; //  complete_para[effective_para_index[i]] = effective_para[i], ohter location of complete_para is 0
     Eigen::VectorXi compute_para_index; //  when its size is zero, compute_para equals to effective_para
 public:
-    UniversalData(int dim, universal_function function, void* data = NULL);
-    UniversalData(const UniversalData& original, const Eigen::VectorXi& target_para_index);
+    UniversalData(int dim, universal_function function, void* data);
+    UniversalData(const UniversalData& original, const Eigen::VectorXi& target_para_index); // update effective_para accroding to target_para_index
     void get_compute_para(const Eigen::VectorXd& effective_para, Eigen::VectorXd& compute_para) const; // extract compute_para from effective_para
     optim_function get_optim_function() const; // create a function which can be optimized by OptimLib
-    void gradient(const Eigen::VectorXd& effective_para, Eigen::VectorXd& gradient);
-    void hessian(const Eigen::VectorXd& effective_para, Eigen::MatrixXd& hessian);
-    void hessian(const Eigen::VectorXd& effective_para, Eigen::VectorXd& gradient, Eigen::MatrixXd& hessian, int index, int size);
+    void gradient(const Eigen::VectorXd& effective_para, Eigen::VectorXd& gradient); // compute the gradient of effective_para
+    void hessian(const Eigen::VectorXd& effective_para, Eigen::MatrixXd& hessian); // compute the hessian of effective_para
+    void hessian(const Eigen::VectorXd& effective_para, Eigen::VectorXd& gradient, Eigen::MatrixXd& hessian, int index, int size); // compute the hessian of sequence from index to (index+size-1) in effective_para
  
 };
 
@@ -34,6 +34,3 @@ typedef std::function<double(const Eigen::VectorXd& effective_para, const Univer
 typedef std::function<double(const Eigen::VectorXd& effective_para, Eigen::VectorXd* gradient, void* data)> optim_function;
 
 #endif
-
-// Eigen::VectorXd get_complete_para(const Eigen::VectorXd& effective_para) const; // tune effective para into complete para by adding 0
- // void get_effective_para(const Eigen::VectorXd& complete_para, Eigen::VectorXd& effective_para) const; // extract effective para from complete_para
