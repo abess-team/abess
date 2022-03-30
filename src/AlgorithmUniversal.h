6a864279a51cc2e42573ce96c191756bbed28e5b
bbayukari <40588568+bbayukari@users.noreply.github.com>
2022-03-31 00:39:43 +0800

    implement Class abessUniversal

--- src/AlgorithmUniversal.h
+++ src/AlgorithmUniversal.h
@@ -5,6 +5,8 @@
 #include"UniversalData.h"
 
 class abessUniversal : public Algorithm<int, Eigen::VectorXd, int, UniversalData> {
+private:
+    double enough_small = 1e-9;
 public:
     abessUniversal(int sample_size, int model_size, int max_iter = 30, int primary_model_fit_max_iter = 10,
         double primary_model_fit_epsilon = 1e-8, bool warm_start = true, int exchange_num = 5,
@@ -13,18 +15,53 @@ public:
             6, UNIVERSAL_MODEL, max_iter, primary_model_fit_max_iter, primary_model_fit_epsilon, warm_start,
             exchange_num, always_select, splicing_type, sub_search) {};
 
-    double loss_function(UniversalData& X, int& y, Eigen::VectorXd& weights, Eigen::VectorXd& beta, int& coef0, Eigen::VectorXi& A,
+    /**
+      * Compute the loss of active_data with L2 penalty, where the value of parameter is active_para.
+      * Only these three paras will be used.  
+      * @param active_data                                          UniversalData which has been limited to active set A
+      * @param active_para                                          the value of effective parameters in active_data
+      * @param lambda                                               L2 penalty coefficient
+      * 
+      * @return a double value indicating the loss
+      */
+    double loss_function(UniversalData& active_data, int& y, Eigen::VectorXd& weights, Eigen::VectorXd& active_para, int& coef0, Eigen::VectorXi& A,
         Eigen::VectorXi& g_index, Eigen::VectorXi& g_size, double lambda);
 
-    void sacrifice(UniversalData& X, UniversalData& XA, int& y, Eigen::VectorXd& beta, Eigen::VectorXd& beta_A, int& coef0, Eigen::VectorXi& A,
-        Eigen::VectorXi& I, Eigen::VectorXd& weights, Eigen::VectorXi& g_index,
-        Eigen::VectorXi& g_size, int N, Eigen::VectorXi& A_ind, Eigen::VectorXd& bd,
-        Eigen::VectorXi& U, Eigen::VectorXi& U_ind, int num);
-
-    bool primary_model_fit(UniversalData& X, int& y, Eigen::VectorXd& weights, Eigen::VectorXd& beta, int& coef0, double loss0,
+    /**
+      * optimize the loss of active_data with L2 penalty  
+      * Only these two paras will be used.
+      * @param active_data                                          UniversalData which will be optimized, it has been limited to active set A
+      * @param active_para                                          a column vector of initial values
+      *
+      * @return a boolean value indicating successful completion of the optimization algorithm.
+      */
+    bool primary_model_fit(UniversalData& active_data, int& y, Eigen::VectorXd& weights, Eigen::VectorXd& active_para, int& coef0, double loss0,
         Eigen::VectorXi& A, Eigen::VectorXi& g_index, Eigen::VectorXi& g_size);
 
-    double effective_number_of_parameter(UniversalData& X, UniversalData& XA, int& y, Eigen::VectorXd& weights, Eigen::VectorXd& beta, Eigen::VectorXd& beta_A,
+    /**
+      * compute the sacrifice of data
+      * Only these seven paras will be used.
+      * @param data                                                 UniversalData which include both active set A and inactive set I
+      * @param para                                                 the value of effective parameters in data
+      * @param A                                                    the index in g_index of group in active set 
+      * @param I                                                    the index in g_index of group in inactive set
+      * @param g_index                                              the index in para of all groups
+      * @param g_size                                               the length of all groups
+      * @param sacrifice                                            a column vector which will be replaced by results
+      */
+    void sacrifice(UniversalData& data, UniversalData& XA, int& y, Eigen::VectorXd& para, Eigen::VectorXd& beta_A, int& coef0, Eigen::VectorXi& A,
+        Eigen::VectorXi& I, Eigen::VectorXd& weights, Eigen::VectorXi& g_index,
+        Eigen::VectorXi& g_size, int g_num, Eigen::VectorXi& A_ind, Eigen::VectorXd& sacrifice,
+        Eigen::VectorXi& U, Eigen::VectorXi& U_ind, int num);
+    /**
+      * compute the effective number of parameters which will be used to compute information criterion
+      * Only these two paras will be used.
+      * @param active_data                                          UniversalData which has been limited to active set A
+      * @param active_para                                          the value of effective parameters in active_data
+      *
+      * @return a double value indicating the effective number of parameters
+      */
+    double effective_number_of_parameter(UniversalData& X, UniversalData& active_data, int& y, Eigen::VectorXd& weights, Eigen::VectorXd& beta, Eigen::VectorXd& active_para,
         int& coef0);
 };
 
--- src/AlgorithmUnivesal.cpp
+++ src/AlgorithmUnivesal.cpp
@@ -1,26 +1,74 @@
 #include "AlgorithmUniversal.h"
+#define OPTIM_ENABLE_EIGEN_WRAPPERS
+#include "OptimLib/optim.hpp"
 
 using namespace std;
 using namespace Eigen;
+using namespace optim;
 
-double abessUniversal::loss_function(UniversalData& X, int& y, VectorXd& weights, VectorXd& beta, int& coef0, VectorXi& A,
-    VectorXi& g_index, VectorXi& g_size, double lambda) {
-
+double abessUniversal::loss_function(UniversalData& active_data, int& y, Eigen::VectorXd& weights, Eigen::VectorXd& active_para, int& coef0, Eigen::VectorXi& A,
+    Eigen::VectorXi& g_index, Eigen::VectorXi& g_size, double lambda) 
+{
+    return active_data.loss(active_para, lambda);
 }
 
-void abessUniversal::sacrifice(UniversalData& X, UniversalData& XA, int& y, VectorXd& beta, VectorXd& beta_A, int& coef0, VectorXi& A,
-    VectorXi& I, VectorXd& weights, VectorXi& g_index,
-    VectorXi& g_size, int N, VectorXi& A_ind, VectorXd& bd,
-    VectorXi& U, VectorXi& U_ind, int num) {
-
-}
-
-double abessUniversal::effective_number_of_parameter(UniversalData& X, UniversalData& XA, int& y, VectorXd& weights, VectorXd& beta, VectorXd& beta_A,
-    int& coef0) {
-
-}
-
-bool abessUniversal::primary_model_fit(UniversalData& X, int& y, VectorXd& weights, VectorXd& beta, int& coef0, double loss0,
+bool abessUniversal::primary_model_fit(UniversalData& active_data, int& y, VectorXd& weights, VectorXd& active_para, int& coef0, double loss0,
     VectorXi& A, VectorXi& g_index, VectorXi& g_size) {
-
+    optim_function opt_objfn = active_data.get_optim_function(this->lambda_level);
+    return lbfgs(active_para, opt_objfn, NULL);
 }
+
+void abessUniversal::sacrifice(UniversalData& data, UniversalData& XA, int& y, VectorXd& para, VectorXd& beta_A, int& coef0, VectorXi& A, VectorXi& I, VectorXd& weights, VectorXi& g_index, VectorXi& g_size, int g_num, VectorXi& A_ind, VectorXd& sacrifice, VectorXi& U, VectorXi& U_ind, int num) 
+{
+    for (int i = 0; i < A.size(); i++) {
+        VectorXd gradient_group(g_size(A[i]));
+        MatrixXd hessian_group(g_size(A[i]), g_size(A[i]));
+        data.hessian(para, gradient_group, hessian_group, g_index(A[i]), g_size(A[i]), this->lambda_level);
+        if (g_size(A[i]) == 1) {
+            // Optimize for degradation situations, it often happens
+            sacrifice(A[i]) = beta(g_index(A[i])) * beta(g_index(A[i])) * hessian_group(0, 0);
+        }
+        else {
+            sacrifice(A[i]) = (para.segment(g_index(A[i]), g_size(A[i])).transpose() * hessian_group * para.segment(g_index(A[i]), g_size(A[i])) / g_size(A[i])).eval()(0,0);
+        }
+    }
+    for (int i = 0; i < I.size(); i++) {
+        VectorXd gradient_group(g_size(I[i]));
+        MatrixXd hessian_group(g_size(I[i]), g_size(I[i]));
+        data.hessian(para, gradient_group, hessian_group, g_index(I[i]), g_size(I[i]), this->lambda_level);
+        if (g_size(I[i]) == 1) {
+            // Optimize for degradation situations, it often happens
+            if (hessian_group(0, 0) < this->enough_small) {
+                cout << "hessian is not positive definite!"; 
+                sacrifice(I[i]) = gradient_group(0, 0)* gradient_group(0, 0) / this->enough_small;
+            }
+            else {
+                sacrifice(I[i]) = gradient_group(0, 0) * gradient_group(0, 0) / hessian_group(0, 0);
+            }
+        }
+        else {
+            //? TODO: hessian may be not positive definite
+            MatrixXd inv_hessian_group = hessian_group.ldlt().solve(MatrixXd::Identity(g_size(i), g_size(i)));
+            sacrifice(I[i]) = (gradient_group.transpose() * inv_hessian_group * gradient_group / g_size(A[i])).eval()(0, 0);
+        }
+    }
+}
+
+double abessUniversal::effective_number_of_parameter(UniversalData& X, UniversalData& active_data, int& y, VectorXd& weights, VectorXd& beta, VectorXd& active_para, int& coef0)
+{
+    if (this->lambda_level == 0.) return active_data.cols();
+
+    if (active_data.cols() == 0) return 0.;
+
+    MatrixXd hessian(active_data.cols(), active_data.cols());
+    active_data.hessian(active_para, hessian, this->lambda_level); //? TODO: need lambda or not?
+    SelfAdjointEigenSolver<MatrixXd> adjoint_eigen_solver(hessian);
+    double enp = 0.;
+    for (int i = 0; i < adjoint_eigen_solver.eigenvalues().size(); i++) {
+        enp += adjoint_eigen_solver.eigenvalues()(i) / (adjoint_eigen_solver.eigenvalues()(i) + this->lambda_level);
+    }
+    return enp;
+}
+
+
+
