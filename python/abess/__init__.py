5ab97721f6e03dabd7e7b17b44d71437f9b3572f
oooo26 <junhao.huang.me@gmail.com>
2023-03-15 15:47:47 +0800

    Revert cibuildwheel to v2.3.1

--- python/abess/__init__.py
+++ python/abess/__init__.py
@@ -5,7 +5,7 @@
 # @Site    :
 # @File    : __init__.py
 
-__version__ = "0.4.6"
+__version__ = "0.4.7rc1"
 __author__ = ("Jin Zhu, Kangkang Jiang, "
               "Junhao Huang, Yanhang Zhang, "
               "Yanhang Zhang, Shiyun Lin, "
