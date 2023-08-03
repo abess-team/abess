4cf94fb113752a699c660ebe7b916e9326bc1600
oooo26 <junhao.huang.me@gmail.com>
2023-08-03 15:55:16 +0800

    Implement clipping on beta

--- python/abess/bess_base.py
+++ python/abess/bess_base.py
@@ -1,3 +1,4 @@
+import sys
 import numbers
 import warnings
 import numpy as np
@@ -207,7 +208,9 @@ class bess_base(BaseEstimator):
             is_normal=True,
             sample_weight=None,
             cv_fold_id=None,
-            sparse_matrix=False):
+            sparse_matrix=False,
+            beta_low=None,
+            beta_high=None):
         r"""
         The fit function is used to transfer
         the information of data and return the fit result.
@@ -583,6 +586,12 @@ class bess_base(BaseEstimator):
             always_select_list = np.zeros(0, dtype="int32")
         else:
             always_select_list = np.array(self.always_select, dtype="int32")
+        
+        # beta range
+        if beta_low is None:
+            beta_low = -sys.float_info.max
+        if beta_high is None:
+            beta_high = sys.float_info.max
 
         # unused
         n_lambda = 100
@@ -607,7 +616,7 @@ class bess_base(BaseEstimator):
                 self.primary_model_fit_epsilon, early_stop,
                 self.approximate_Newton, self.thread, self.covariance_update,
                 sparse_matrix, self.splicing_type, self.important_search,
-                A_init_list, self.fit_intercept)
+                A_init_list, self.fit_intercept, beta_low, beta_high)
 
         self.coef_ = result[0].squeeze()
         self.intercept_ = result[1].squeeze()
